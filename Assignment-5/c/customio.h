ad5a34db44a73a05bb6e6186f5890f922ca4b0c0
Tsubasa <J16N@users.noreply.github.com>
2021-10-22 09:34:53 +0000

    Updated

---
+++ Assignment-5/c/customio.h
@@ -0,0 +1,7 @@
+#ifndef CUSTOMIO_H
+#define CUSTOMIO_H
+
+int *get_int_array(char *, int *);
+void print_int_array(char *, int *, int);
+
+#endif
---
+++ Assignment-5/c/p2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "customio.h"
+
+int main(void)
+{
+    int len;
+    int *arr = get_int_array("Array: ", &len);
+
+    
+
+    return 0;
+}
